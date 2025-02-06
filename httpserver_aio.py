92c77261e364a70007bf36c18bb10628675075b8
Markus <markus.schneider@katek-group.com>
2025-02-06 14:59:18 +0100

    refactor: getChunkData leicht angepasstt.

--- httpserver_aio.py
+++ httpserver_aio.py
@@ -2,20 +2,22 @@ from aiohttp import web
 import asyncio
 import aiofiles
 
-chunk_size = 512
+chunk_size = 8895
 
 async def get_chunk_data(): 
-    async with aiofiles.open('./get_orders_response.xml', 'rb') as f: 
-        while chunk := await f.read(chunk_size): 
-            chunk_length = f"{len(chunk):x}\r\n"
-            chunk_length = chunk_length.encode('utf8')
+    async with aiofiles.open('./get_orders_response_rn.xml', 'rb') as f: 
+        while chunk := await f.read(512): 
+            #chunk_length = f"{len(chunk):x}\r\n"
+            #chunk_length = chunk_length.encode('utf8')
+            chunk_length = f"{chunk_size}:\r\n".encode('utf8')
             yield chunk_length
+            #chunk[chunk_size] += '\r\n'
             yield chunk
             yield b'\r\n'
             await asyncio.sleep(0.1)
 
 async def get_chunk_data2(): 
-    async with aiofiles.open('./get_orders_response.xml', 'rb') as f: 
+    async with aiofiles.open('./get_orders_response.txt', 'rb') as f: 
         while chunk := await f.read(chunk_size): 
             yield chunk
             await asyncio.sleep(0.1)
@@ -35,7 +37,7 @@ async def handle_get(request):
         'Transfer-Encoding': 'chunked'
     })
 
-    response.enable_chunked_encoding()
+    # response.enable_chunked_encoding()
     await response.prepare(request)
     
 
@@ -47,19 +49,19 @@ async def handle_get(request):
     # Daten in Chunks senden
 
 
-    async for chunk in get_chunk_data():
+    async for chunk in get_chunk_data2():
         await response.write(chunk)
         
 
 
     # # Response abschließen
-    await response.write(b'0\r\n\r\n')
+    #await response.write(b'0\r\n\r\n')
     await response.write_eof()
     return response
 
 
 app = web.Application()
-app.add_routes([web.get('/getOrdersResponse', handle_get)])
+app.add_routes([web.post('/getOrdersResponse', handle_get)])
 
 if __name__ == '__main__':
     web.run_app(app, host='0.0.0.0', port=3030)
